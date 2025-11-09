8745873cbc8afbd6544b391577e37e98e2a18156
JungHaeIn <ini0617@naver.com>
2025-11-09 10:24:05 +0900

    refactor: 레이아웃 기본 구조 및 폭 고정 설정

--- src/shares/layouts/Layout.tsx
+++ src/shares/layouts/Layout.tsx
@@ -2,15 +2,21 @@ import { Outlet } from "react-router-dom";
 import Header from "@/shares/layouts/Header";
 import { Footer } from "@/shares/layouts/Footer";
 
-//레이아웃 컴포넌트
+// 전체 레이아웃 컴포넌트
 export function Layout() {
   return (
-    <>
+    // 모바일에서는 꽉 차게, 웹에서는 672px로 고정 + 중앙 정렬
+    <div className="flex flex-col w-full max-w-2xl min-h-screen mx-auto bg-white">
+      {/* 헤더 */}
       <Header />
-      <main className="min-h-[calc(100vh-200px)] px-4 py-8">
-        <Outlet /> {/* 각 페이지가 여기로 들어감 */}
+
+      {/* 메인 콘텐츠 영역 */}
+      <main className="flex-1 min-h-[calc(100vh-200px)] px-4 py-8">
+        <Outlet /> {/* 각 페이지 내용 */}
       </main>
+
+      {/* 푸터 */}
       <Footer />
-    </>
+    </div>
   );
 }
