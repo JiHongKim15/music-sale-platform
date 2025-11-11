20c6bada57ee036d64e2f4b501f18dd2e2639342
정해인 <ini0617@naver.com>
2025-11-11 17:30:08 +0900

    fix: 컴포넌트 오타 수정

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,3 +1,3 @@
-export default function CommunityPage() {
-  return <div className="p-4">지도 페이지 (CommunityPage)</div>;
+export default function MapPage() {
+  return <div className="p-4">지도 페이지 (MapPage)</div>;
 }
