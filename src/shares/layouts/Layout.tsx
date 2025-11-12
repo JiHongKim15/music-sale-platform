0597237aba1585401a9f9e8bb1d6ec20ca20a963
Jiwon Chae <63784453+jiwon0226@users.noreply.github.com>
2025-11-12 19:55:02 +0900

    fix: 푸터 크기 제한 수정 및 레이아웃 스타일 수정, 코드 안정화

--- src/shares/layouts/Layout.tsx
+++ src/shares/layouts/Layout.tsx
@@ -1,27 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/shares/layouts/Header";
-import { Footer } from "@/shares/layouts/Footer";
+import Footer from "@/shares/layouts/Footer";
 
 // 전체 레이아웃 컴포넌트
 export function Layout() {
   return (
     <div className="flex justify-center w-full min-h-screen bg-gray-50">
-      {/* 중앙 고정된 672px 박스 */}
-      <div className="relative flex flex-col w-[672px] min-h-screen bg-white">
+      {/* 중앙 고정된 박스 */}
+      <div className="relative flex flex-col w-[672px] min-h-screen shadow-[0_4px_20px_rgba(0,0,0,0.1)] bg-white">
         {/* 헤더 */}
-        <header className="sticky top-0 z-50 bg-white">
-          <Header />
-        </header>
+        <Header />
 
         {/* 메인 */}
-        <main className="flex-1 overflow-y-auto px-4 py-8">
+        <main className="flex-1 overflow-y-auto px-4 pt-[120px] pb-[90px]">
           <Outlet />
         </main>
-
         {/* 푸터 */}
-        <footer className="w-full border-t border-gray-200 bg-white">
-          <Footer />
-        </footer>
+        <Footer />
       </div>
     </div>
   );
